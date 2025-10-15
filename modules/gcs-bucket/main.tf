47a289e971b3440e11c3bbb72be3ef1083d0649b
Saurabh Agarwal <saurabhagarwal@Saurabhs-MacBook-Pro.local>
2025-10-16 04:08:15 +0530

    fixed validation errors

--- modules/gcs-bucket/main.tf
+++ modules/gcs-bucket/main.tf
@@ -17,6 +17,7 @@ resource "google_storage_bucket" "bucket" {
   project       = var.project_id
 
   uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
 
   versioning {
     enabled = var.versioning_enabled
