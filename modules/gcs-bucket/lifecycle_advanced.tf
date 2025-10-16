63137c61f2b683418bb8fe38791549845021f123
Saurabh Agarwal <saurabhagarwal@Saurabhs-MacBook-Pro.local>
2025-10-16 12:20:40 +0530

    feat: Add advanced lifecycle management rules
    - Archive objects to ARCHIVE class after 90 days
    - Abort incomplete multipart uploads after 7 days

---
+++ modules/gcs-bucket/lifecycle_advanced.tf
@@ -0,0 +1,27 @@
+# Advanced lifecycle rules
+resource "google_storage_bucket_lifecycle_rule" "archive_old_versions" {
+  bucket = google_storage_bucket.bucket.name
+
+  action {
+    type          = "SetStorageClass"
+    storage_class = "ARCHIVE"
+  }
+
+  condition {
+    age                   = 90
+    with_state            = "ARCHIVED"
+    matches_storage_class = ["STANDARD"]
+  }
+}
+
+resource "google_storage_bucket_lifecycle_rule" "delete_incomplete_uploads" {
+  bucket = google_storage_bucket.bucket.name
+
+  action {
+    type = "AbortIncompleteMultipartUpload"
+  }
+
+  condition {
+    age = 7
+  }
+}
